7a0abd72473f995cbe921ca37f9fff700bf121c9
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-07-02 22:47:32 +0500

    ewf

--- Db/Db.connection.js
+++ Db/Db.connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async()=>{
     try{
-       mongoose.connect('mongodb://localhost:27017/LibarayManager',{
+       mongoose.connect('mongodb+srv://chabdullah:abdullah21@backend.tr2ys.mongodb.net/?retryWrites=true&w=majority&appName=backend',{
            
         })
         console.log('Database connected successfully')
